abf6feb4af7e5160de61e11be16c484d97a6a09d
PeregrinAl <parshintseva-s@mail.ru>
2022-12-28 22:08:20 +0500

    modified:   web/lib/screens/home/home_screen.dart

--- web/lib/screens/home/home_screen.dart
+++ web/lib/screens/home/home_screen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:web/constants.dart';
+import 'package:web/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
+      body: Body(),
     );
   }
 
   AppBar buildAppBar() {
     return AppBar(
+        backgroundColor: firstColor,
         elevation: 0,
         leading: IconButton(
           icon: SvgPicture.asset("assets/icons/icon1.svg"),
